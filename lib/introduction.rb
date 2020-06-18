4a3bce7d49d8e41f8815293faab4a80297f22077
Antonio O Fong <omarquinzan@yahoo.com>
2020-06-18 02:45:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,4 +3,5 @@ def introduction name
   puts "Hi, my name is #{name}."
 end
 def introduction_with_language(name, language )
-  
+  puts "hi, my name is Dan and I am learning to program in #{language}\n"
+end
